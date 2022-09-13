ef3e00db1b1fabea250ab3707555f4143714ed7c
Johann Kergal <hello@johannkergal.fr>
2022-09-13 11:55:09 +0200

    Move database utils and manager methods to separate files

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
-const express = require('express')
-const typeorm = require('typeorm')
-const Wilder = require('./models/Wilder')
+const express = require('express');
+const { getDatabase } = require('./database/utils');
+const { initializeWilders } = require('./models/Wilder/manager');
 
-const app = express()
-const PORT = 4000
+const app = express();
 
-const dataSource = new typeorm.DataSource({
-    type: "sqlite",
-    database: './wildersdb.sqlite',
-    synchronize: true,
-    entities: [Wilder],
-    logging: ["query", "error"],
+app.get('/', function (req, res) {
+	res.send('Hello world from Express!');
 });
 
-app.get("/", (req, res) => {
-    res.send('Hello world, from Express !')
-});
+const PORT = 4000;
 
-const start = async () => {
-    await dataSource.initialize();
-    await dataSource.getRepository(Wilder).clear();
-    await dataSource.getRepository(Wilder).save({ name: 'Jean Wilder' })
-
-    app.listen(PORT, () => {
-        console.log(`Server running on PORT ${PORT} ✅`)
-    })
+async function start() {
+	await initializeWilders();
+	await getDatabase();
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT} 👍`);
+	});
 }
 
-start()
+start();
