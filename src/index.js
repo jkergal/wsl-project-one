7cf5c3e013dfa5becde7ff11e570c87fdcac42bd
Johann Kergal <hello@johannkergal.fr>
2022-09-13 14:49:17 +0200

    Add method POST /wilders to create new wilder

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ const { getDatabase } = require('./database/utils');
 const { initializeWilders } = require('./models/Wilder/manager');
 
 const app = express();
+const PORT = 4000;
+const WILDERS_PATH = '/wilders';
+
+app.use(express.json());
 
 app.get('/', function (req, res) {
 	res.send('Hello world from Express!');
 });
-app.get('/wilders', wildersControllers.get);
 
-const PORT = 4000;
+app.get(WILDERS_PATH, wildersControllers.get);
+app.post(WILDERS_PATH, wildersControllers.post);
 
 async function start() {
 	await initializeWilders();
