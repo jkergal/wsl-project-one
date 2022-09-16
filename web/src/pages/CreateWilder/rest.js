7585e4f20b70871ac0b0b93fbf522e7ffe426364
Johann Kergal <hello@johannkergal.fr>
2022-09-16 12:40:59 +0200

    Refactor queries to REST API

--- web/src/pages/CreateWilder/rest.js
+++ web/src/pages/CreateWilder/rest.js
@@ -1,21 +1,5 @@
+import { query, WILDERS_PATH } from 'services/rest';
+
 export async function createWilder(firstName, lastName, isTrainer) {
-	let httpStatusError = false;
-	try {
-		const response = await fetch('/wilders', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ firstName, lastName, isTrainer })
-		});
-		const message = await response.json();
-		if (!response.ok) {
-			httpStatusError = true;
-			throw Error(message.error);
-		}
-		return message;
-	} catch (error) {
-		if (httpStatusError) {
-			throw Error(error.message);
-		}
-		throw Error('Impossible de joindre le serveur. Vérifiez votre connexion à Internet.');
-	}
+	return query(WILDERS_PATH, 'POST', { firstName, lastName, isTrainer });
 }
