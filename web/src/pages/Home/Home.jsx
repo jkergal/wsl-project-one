ee489508965ba003f3e50e447f29f7699f1186ab
Johann Kergal <hello@johannkergal.fr>
2022-09-16 12:47:35 +0200

    Show error message if wilders cannot be loaded

--- web/src/pages/Home/Home.jsx
+++ web/src/pages/Home/Home.jsx
@@ -1,46 +1,62 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
-import { CardRow } from "pages/Home/Home.styled";
-import Wilder from "components/Wilder/Wilder";
+import { CardRow } from 'pages/Home/Home.styled';
+import Wilder from 'components/Wilder/Wilder';
 import { LoadingSpinner } from 'components/Loader/Loader.styled';
-import { SectionTitle } from "styles/base-styles";
-import { CREATE_WILDER_PATH } from "paths";
-import { fetchWilders } from "./rest";
+import { SectionTitle } from 'styles/base-styles';
+import { CREATE_WILDER_PATH } from 'paths';
+import { fetchWilders } from './rest';
 
 const Home = () => {
 	const [wilders, setWilders] = useState(null);
 	const [isLoading, setIsLoading] = useState(true);
+	const [errorMessage, setErrorMessage] = useState('');
 
 	useEffect(() => {
 		(async () => {
-			const fetchedWilders = await fetchWilders();
-			setWilders(fetchedWilders);
-			setIsLoading(false);
+			try {
+				const fetchedWilders = await fetchWilders();
+				setWilders(fetchedWilders);
+			} catch (error) {
+				setErrorMessage(error.message);
+			} finally {
+				setIsLoading(false);
+			}
 		})();
 	}, []);
+
+	const renderMainContent = () => {
+		if (isLoading) {
+			return <LoadingSpinner />;
+		}
+		if (errorMessage) {
+			return errorMessage;
+		}
+		if (!wilders?.length) {
+			return "Pas de Wilder... Rejoint-nous! 😎";
+		}
+		return (
+			<CardRow>
+				{wilders?.map((wilder) => (
+					<Wilder
+						key={wilder.id}
+						firstName={wilder.firstName}
+						lastName={wilder.lastName}
+						skills={wilder.skills}
+					/>
+				))}
+			</CardRow>
+		);
+	};
 
 	return (
 		<>
 			<SectionTitle>Wilders</SectionTitle>
 			<Link to={CREATE_WILDER_PATH}>Ajouter un nouveau Wilder</Link>
-				{isLoading ? (
-					<LoadingSpinner />
-				) : wilders.length === 0 ? (
-					"Il n'y a pas encore de Wilders ! Rejoint-nous 😎"
-				) : (
-					<CardRow>
-						{wilders?.map((wilder) => (
-							<Wilder
-								key={wilder.id}
-								firstName={wilder.firstName}
-								lastName={wilder.lastName}
-								skills={wilder.skills}
-								isTrainer={wilder.isTrainer}
-							/>
-						))}
-					</CardRow>
-				)}
+			<br />
+			<br />
+			{renderMainContent()}
 		</>
 	);
 };
