54ced412263ba9bedab12be6ee9f24d68ca03645
Johann Kergal <hello@johannkergal.fr>
2022-09-23 13:22:23 +0200

    Create AddSkill component

--- web/src/components/AddSkill/AddSkill.tsx
+++ web/src/components/AddSkill/AddSkill.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import Button from 'components/Button/Button';
+import Input from 'components/Input/Input';
+import React, { ChangeEvent, useState } from 'react';
+import { FormStyled, Label, SectionTitle } from 'styles/base-styles';
 
 const AddSkill = () => {
-	return <div>AddSkill</div>;
+	const [skill, setSkill] = useState('');
+
+	return (
+		<>
+			<SectionTitle>Créer une compétence</SectionTitle>
+			<FormStyled>
+				<Label>
+					Skill
+					<br />
+					<Input
+						placeholder="JavaScript"
+						inputType="text"
+						value={skill}
+						onChange={(event: ChangeEvent<HTMLInputElement>) => {
+							setSkill(event.target.value);
+						}}
+					/>
+				</Label>
+
+				<Button label="Valider"></Button>
+			</FormStyled>
+		</>
+	);
 };
 
 export default AddSkill;
